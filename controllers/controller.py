960347fdc46df3967a8a6aa3c90ac818da8efb54
Lewis <lewisdsykes@gmail.com>
2022-07-10 20:29:19 +0100

    added function

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,14 +4,18 @@ from models.player import Player
 from models.game import Game
 
 
+# @app.route("/players")
+# def index():
+#     return render_template("index.html", players=players)
+
+
 @app.route("/<player1choice>/<player2choice>")
-def index(player1choice, player2choice): 
+def check_win(player1choice, player2choice):
     player1 = Player(player1choice)
     player2 = Player(player2choice)
     new_game = Game(player1, player2)
     result = new_game.check_winner()
     return render_template("index.html", result=result)
-
 
 
 
--- models/game.py
+++ models/game.py
@@ -1,7 +1,19 @@
-class Game:
+from secrets import choice
+from models.player import Player
+from models.player_list import *
+
+class Game: 
     def __init__(self, _player1, _player2):
         self.player1 = _player1
-        self.player2 = _player2 
+        self.player2 = _player2
 
     def check_winner(self):
-        return "winner"
+        if player_1.choice == "Rock" and player_2.choice == "Scissors":
+            return "Player 1 Wins"
+        elif player_1.choice == "Scissors" and player_2.choice == "Rock":
+            return "Player 2 Wins"
+        else: 
+            return None
+
+
+
